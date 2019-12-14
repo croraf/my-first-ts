cbd71556103b578d0ef0f2f0f714d709c503a26c
croraf <vrafaeli@msn.com>
2019-12-14 17:32:17 +0100

    Remove .js files

--- src/greeter.js
+++
@@ -1,8 +0,0 @@
-/* import {getCurrentTime} from './util.js'; */
-const greeter = async (person) => {
-    const { getCurrentTime } = await import('./util.js');
-    console.log('util:', getCurrentTime);
-    const currentTime = getCurrentTime();
-    return "Hello, " + person + ', ' + currentTime;
-};
-export { greeter };
